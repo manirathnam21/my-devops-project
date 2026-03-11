d89b53dce91eaafb27dd68a6856ee9a9f9ef7bc6
EC2 Default User <ec2-user@ip-172-31-89-109.ec2.internal>
2026-03-11 10:59:04 +0000

    pylint

--- electiononlineapp/apps.py
+++ electiononlineapp/apps.py
@@ -1,6 +1,6 @@
 """Django apps here"""
 from django.apps import AppConfig
-=
+
 class ElectiononlineappConfig(AppConfig):
     """Election Line app"""
     default_auto_field = 'django.db.models.BigAutoField'
